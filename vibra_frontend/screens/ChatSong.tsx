fdf131e74e94459894186a77e909e25bbfeedf3a
emiliakallis <emilia.kallis@hotmail.com>
2024-11-02 14:54:49 +0100

    Revert "Emilia"

--- vibra_frontend/screens/ChatSong.tsx
+++ vibra_frontend/screens/ChatSong.tsx
@@ -1,27 +1,19 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { FlatList, StyleSheet, TouchableOpacity, SafeAreaView, Dimensions, ViewToken, LayoutChangeEvent } from 'react-native';
-import { useRoute, useNavigation, useFocusEffect, RouteProp } from '@react-navigation/native';
+import { FlatList, StyleSheet, TouchableOpacity, SafeAreaView, Dimensions } from 'react-native';
+import { useRoute, useNavigation, useFocusEffect } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import SongCard from '@/components/SongCard';
 import NowPlayingBar from '@/components/NowPlayingBar';
 import { stopAudio, playSong } from '@/services/AudioService';
-import { Message } from '@/types/Message';
-import LikedByItem from '@/components/LikedByItem';
 import { View, Text } from '@/components/Themed';
 import { GradientView } from '@/components/GradientView';
 import { Audio } from 'expo-av';
 
 const { height } = Dimensions.get('window');
 
-const getItemLayout = (data: ArrayLike<any> | null | undefined, index: number) => ({
-  length: height,
-  offset: height * index,
-  index,
-});
-
 const SongDetail = () => {
-  const route = useRoute<RouteProp<{ params: { selectedMessage: any; songMessages: any[] } }, 'params'>>();
+  const route = useRoute();
   const navigation = useNavigation();
   const { selectedMessage, songMessages } = route.params;
 
@@ -29,30 +21,6 @@ const SongDetail = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [cardHeight, setCardHeight] = useState(height);
   const soundRef = useRef(null);
-  const [gradientColors, setGradientColors] = useState<string[]>(['transparent']);
-
-  // Utility function to convert RGB array to hex color string
-  const rgbToHex = (rgb: number[] = []): string => {
-    return '#' + rgb.map(value => value.toString(16).padStart(2, '0')).join('');
-  };
-
-  // Function to set gradient colors from track properties
-  const updateGradientColors = (palette: Array<number[]> | undefined, dominantColor: number[] | undefined) => {
-    const dominantColorHex = dominantColor ? rgbToHex(dominantColor) : 'transparent';
-    const paletteHex = palette ? palette.map(rgb => rgbToHex(rgb)) : [];
-
-    // If the palette has colors, use it; otherwise, fallback to the dominant color and transparency
-    const colors = paletteHex.length > 1 ? paletteHex : [dominantColorHex, 'transparent'];
-    setGradientColors(colors);
-  };
-
-  useEffect(() => {
-    // Update gradient when the current track changes
-    if (currentTrack) {
-      updateGradientColors(currentTrack.album_image_palette, currentTrack.album_image_dominant_color);
-    }
-  }, [currentTrack]);
-
   const flatListRef = useRef<FlatList<Message>>(null);
   const isScrollingRef = useRef(false);
 
@@ -138,34 +106,32 @@ const SongDetail = () => {
   );
 
   return (
-    <LinearGradient colors={gradientColors} style={styles.gradientBackground}>
+    <GradientView style={styles.gradientBackground}>
       <SafeAreaView style={styles.container}>
         <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
           <Ionicons name="arrow-back" size={30} color="white" />
         </TouchableOpacity>
+
         <FlatList
           ref={flatListRef}
           data={songMessages} // Only use the chat's song messages
           keyExtractor={(item, index) => (item.id ? item.id.toString() : index.toString())} // Fallback for key
           renderItem={({ item }) => (
-            <View style={styles.songCardContainer}>
-              <View style={styles.likedByItem}>
-                <LikedByItem liked_by={item.sender}/>
-              </View>
-              <TouchableOpacity onPress={handleTogglePlayPause} onLayout={onCardLayout}>
-                <SongCard
-                  image={{ uri: item.track.album_image }}
-                  title={item.track.track_title}
-                  description={item.track.artist_name}
-                  track_id={item.track.track_id}
-                  isPlaying={currentTrack.track_id === item.track.track_id && isPlaying}
-                  onTogglePlayPause={handleTogglePlayPause}
-                  sender={item.sender.username}
-                  conversations={[]}
-                  imageMargin={130}
+            
+            <TouchableOpacity onPress={handleTogglePlayPause} onLayout={onCardLayout}>
+              <SongCard
+                image={{ uri: item.track.album_image }}
+                title={item.track.track_title}
+                description={item.track.artist_name}
+                track_id={item.track.track_id}
+                isPlaying={currentTrack.track_id === item.track.track_id && isPlaying}
+                onTogglePlayPause={handleTogglePlayPause}
+                palette={item.track.album_image_palette}
+                dominantColor={item.track.album_image_dominant_color} 
+                sender={item.sender.username}
+                conversations={[]}
                 />
-              </TouchableOpacity>
-            </View>
+            </TouchableOpacity>
           )}
           pagingEnabled={true}
           showsVerticalScrollIndicator={false}
@@ -181,6 +147,7 @@ const SongDetail = () => {
             });
           }}
         />
+
         {currentTrack && <View style={styles.playingBar}><NowPlayingBar title={currentTrack.track_title} artist={currentTrack.artist_name}/></View> }
       </SafeAreaView>
     </GradientView>
@@ -188,17 +155,8 @@ const SongDetail = () => {
 };
 
 const styles = StyleSheet.create({
-  songCardContainer: {
-    position: 'relative',
-    backgroundColor: 'transparent'
-  },
-  likedByItem: {
-    position: 'absolute',
-    top: 30, // Adjust this value to control vertical positioning
-    zIndex: 2, // Ensure it stays above SongCard
-  },
   gradientBackground: {
-     flex: 1,
+    flex: 1,
   },
   container: {
     flex: 1,
